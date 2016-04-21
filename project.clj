d9b93d2452d93c7c7fda7cfba324799050567405
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2016-04-21 16:20:33 +0200

    [STRUCTURE] api structure

--- project.clj
+++ project.clj
@@ -1,6 +1,18 @@
-(defproject clj-ovh "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-ovh "0.1.0"
+  :description "lojure OVH Client"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                                  ;; logging stuff
+                                  [org.clojure/tools.logging "0.3.1"]
+                                  [org.slf4j/slf4j-api "1.7.19"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.19"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.19"]
+                                  [log4j "1.2.17"]
+                                  [clj-time "0.11.0"]
+                                  ;;for dd
+                                  [clj-http "2.1.0"]
+                                  [cheshire "5.5.0"]
+                                  [digest "1.4.4"]
+                                  [circleci/clj-yaml "0.5.6"]])
--- src/clj_ovh/core.clj
+++ src/clj_ovh/core.clj
@@ -1,6 +1,49 @@
-(ns clj-ovh.core)
+(ns clj-ovh.core
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [cheshire.core :refer :all]
+            [clj-http.client :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def creds (atom {}))
+
+(def endpoint "https://api.ovh.com/1.0")
+
+(defn timestamp
+  []
+  (str (long (/ (System/currentTimeMillis) 1000))))
+
+(def mk-signature
+  [method query body ts]
+  (let [prefix "$1$"
+        digest-string (str (:app-secret @creds) "+" (:consumer-key @creds) "+" method "+" body "+" ts)]
+    (str prefix (digest/sha1 digest-string))))
+
+(def mk-headers
+  [method query body ts]
+  {"Content-Type" "application/json"
+   "X-Ovh-Application" (:app-key @creds)
+   "X-Ovh-Timestamp" ts
+   "X-Ovh-Signature" (mk-signature method query body ts)
+   "X-Ovh-Consumer" (:consumer-key @creds)})
+
+(defmulti call (fn [params] (:method params)))
+
+(defmethod call "GET" [params]
+  (let [url (str endpoint (:ressource params))
+        ts (timestamp)
+        headers (mk-headers "GET" url "" ts)]))
+
+(defmethod call "PUT" [params])
+
+(defmethod call "POST" [params])
+
+(defmethod call "DELETE" [params])
+
+(defmethod call :default [params])
+
+(defn init!
+  [app-key app-secret consumer-key]
+  (swap! creds assoc :app-key app-key
+                     :app-secret app-secret
+                     :consumer-key consumer-key))

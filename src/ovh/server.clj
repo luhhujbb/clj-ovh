c45cb82a6dfadc108602851a9ee91492a217b445
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2016-04-22 11:41:35 +0200

    [ADD] add vrack and server namespace

---
+++ src/ovh/server.clj
@@ -0,0 +1,5 @@
+(ns ovh.server
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [cheshire.core :refer :all]
+            [ovh.core :as ovh]))
---
+++ src/ovh/vrack.clj
@@ -0,0 +1,5 @@
+(ns ovh.vrack
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [cheshire.core :refer :all]
+            [ovh.core :as ovh]))
